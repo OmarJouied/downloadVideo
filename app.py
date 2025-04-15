d5139ea77e36ccd2eb7d390ecd2ed1a39044bfe7
OmarJouied <omarjouied1421@yahoo.com>
2025-04-15 19:13:02 +0100

    build video downloader api

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template, request, send_file, json
+from flask import Flask, render_template, request, send_file
 from pytubefix import YouTube
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from moviepy.audio.io.AudioFileClip import AudioFileClip
-from io import BytesIO
 
-from video_editing.clip import clip_video, clip_audio, merge_audio_video
-from lib import filter_streams, get_stream
+from video_editing.clip import merge_audio_video
+from lib import get_stream
+
+
+
+import tempfile
+import os
+from random import randint
+
+
+TEMP_DIR = tempfile.gettempdir()
+
+
+
 
 app = Flask(__name__)
 
@@ -13,20 +22,20 @@ app = Flask(__name__)
 def home():
   return render_template("index.html")
 
-@app.route('/info')
+@app.route('/video-info')
 def info():
   # Get the URL of the YouTube video
   url = request.args.get('url')
   
-  yt = YouTube(url, use_po_token=True)
+  yt = YouTube(url)
 
   title = yt.title
-  thumbnail_url = yt.thumbnail_url
+  thumbnail = yt.vid_info["videoDetails"]["thumbnail"]["thumbnails"][-1]
   length = yt.length
-  formats = filter_streams(yt.streams.filter(type="video", mime_type="video/mp4"))
+  formats = list(map(lambda format: {"itag": format.itag, "qualityLabel": format.resolution, "isCompleted": format.is_progressive, "type": format.mime_type.split('/').pop() }, filter(lambda stream: stream.resolution, yt.streams)))
 
   # Send json object contains video data to the browser
-  return { "title": title, "thumbnail_url": thumbnail_url, "length": length,"formats": formats }
+  return { "url": url, "title": title, "thumbnail": thumbnail, "length": length, "formats": formats }
 
 @app.route('/download')
 def download():
@@ -34,32 +43,58 @@ def download():
   url = request.args.get('url')
   itag = request.args.get('itag')
   start = int(request.args.get('start') or 0)
-  end = int(request.args.get('end') or yt.length)
+  end = int(request.args.get('end') or 0) or None
   
-  yt = YouTube(url, use_po_token=True)
+  yt = YouTube(url)
   
   video = yt.streams.get_by_itag(itag)
   file_stream = get_stream(video)
-
+  
   if video.is_progressive and start == 0 and (end is None or end == yt.length):
     return send_file(file_stream, as_attachment=True, download_name=f'{yt.title}.{video.mime_type.split('/').pop()}')
 
-  video_cliped = clip_video(file_stream.read(), start, end)
+  # video_cliped = clip_video(file_stream.read(), start, end)
 
-  if not video.is_progressive:
-    audio_stream = get_stream(yt.streams.get_audio_only())
-    audio_cliped = clip_audio(audio_stream.read(), start, end)
+  # if not video.is_progressive:
+  #   audio_stream = get_stream(yt.streams.get_audio_only())
+  #   audio_cliped = clip_audio(audio_stream.read(), start, end)
 
-  result = merge_audio_video(
-    audio_cliped,
-    video_cliped,
-  ) if not video.is_progressive else video_cliped
+  rand_num = randint(0, 1000000)
+  video_path = os.path.join(TEMP_DIR, f"{rand_num}_video.mp4")
+  audio_path = os.path.join(TEMP_DIR, f"{rand_num}_audio.mp3")
+  output_path = os.path.join(TEMP_DIR, f"{rand_num}_output.mp4")
 
-  return send_file(result, as_attachment=True, download_name=f'{yt.title}.{video.mime_type.split('/').pop()}')
+  with open(video_path, 'wb') as f:
+    f.write(file_stream.read())
+
+  with open(audio_path, 'wb') as f:
+    f.write(get_stream(yt.streams.get_audio_only()).read())
+
+  merge_audio_video(
+    video_path,
+    None if video.is_progressive else audio_path,
+    output_path,
+    None if start == 0 else start,
+    None if end is None or end == yt.length else end,
+  )
+
+  response = send_file(output_path, as_attachment=True, download_name=f'{yt.title}.{video.mime_type.split('/').pop()}')
+
+  # Clean up temporary files
+  @response.call_on_close
+  def cleanup():
+    try:
+      os.remove(video_path)
+      os.remove(audio_path)
+      os.remove(output_path)
+    except Exception as e:
+      print(f"Error removing temporary files: {e}")
+
+  return response
 
 @app.route('/error')
 def error():
     return render_template('error.html', message='Please fill in all fields!')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", debug=True)
--- video_editing/clip.py
+++ video_editing/clip.py
@@ -1,49 +1,122 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
-from moviepy.audio.io.AudioFileClip import AudioFileClip
-import tempfile
+# from moviepy.video.io.VideoFileClip import VideoFileClip
+# from moviepy.audio.io.AudioFileClip import AudioFileClip
+# import tempfile
 
-def clip_video(file_content, start, end):
-  with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+# def clip_video(file_content, start, end):
+#   with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
 
-    temp_file.write(file_content)
-    temp_filename = temp_file.name
+#     temp_file.write(file_content)
+#     temp_filename = temp_file.name
 
-  with VideoFileClip(temp_filename) as video_clip:
+#   with VideoFileClip(temp_filename) as video_clip:
 
-      trimmed_video = video_clip.subclip(start, end)
+#       trimmed_video = video_clip.subclipped(start, end)
 
-      with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as trimmed_video_filename:
+#       with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as trimmed_video_filename:
 
-        trimmed_video.write_videofile(trimmed_video_filename.name, codec="libx264")
+#         trimmed_video.write_videofile(trimmed_video_filename.name, codec="libx264")
 
-        # Send the file to the browser
-        return trimmed_video_filename.name
+#         # Send the file to the browser
+#         return trimmed_video_filename.name
 
-def clip_audio(file_content, start, end):
-  with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
+# def clip_audio(file_content, start, end):
+#   with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_file:
 
-    temp_file.write(file_content)
-    temp_filename = temp_file.name
+#     temp_file.write(file_content)
+#     temp_filename = temp_file.name
 
-  with AudioFileClip(temp_filename) as audio_clip:
+#   with AudioFileClip(temp_filename) as audio_clip:
 
-      trimmed_audio = audio_clip.subclip(start, end)
+#       trimmed_audio = audio_clip.subclipped(start, end)
 
-      with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as trimmed_audio_filename:
+#       with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as trimmed_audio_filename:
 
-        trimmed_audio.write_audiofile(trimmed_audio_filename.name)
+#         trimmed_audio.write_audiofile(trimmed_audio_filename.name)
 
-        # Send the file to the browser
-        return trimmed_audio_filename.name
+#         # Send the file to the browser
+#         return trimmed_audio_filename.name
 
-def merge_audio_video(audio_file, video_file):
-  with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+# def merge_audio_video(video_stream, audio_stream=None, start=None, end=None):
+#   with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_video:
 
-    audio = AudioFileClip(audio_file)
-    video = VideoFileClip(video_file)
+#     temp_video.write(video_stream)
+#     video_path = temp_video.name
+    
+#     video_clip = VideoFileClip(video_path)
 
-    video = video.set_audio(audio)
+#     if audio_stream:
+#         with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp_audio:
 
-    video.write_videofile(temp_file.name, codec="libx264")
+#           temp_audio.write(audio_stream)
+#           audio_path = temp_audio.name
+          
+#           audio_clip = AudioFileClip(audio_path)
+#           video_clip = video_clip.with_audio(audio_clip)
+          
+    
+#     if start and end:
+#       video_clip = video_clip.subclipped(start, end)
 
-    return temp_file.name
+#     video_clip.write_videofile(video_path)
+
+#     audio_clip.close()
+#     video_clip.close()
+
+#     return video_path
+
+    
+    
+  
+  
+  
+#   # with tempfile.NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
+
+#     audio = AudioFileClip(audio_file)
+#     video = VideoFileClip(video_file)
+
+#     video = video.with_audio(audio)
+
+#     video.write_videofile(temp_file.name, codec="libx264")
+
+#     return temp_file.name
+
+import ffmpeg
+
+def merge_audio_video(
+    video_path,
+    audio_path,
+    output_path,
+    start=None,
+    end=None,
+):
+  """Merge audio and video files using ffmpeg.
+  Args:
+      video_path (str): Path to the video file.
+      audio_path (str): Path to the audio file.
+      output_path (str): Path to save the merged file.
+      start (int, optional): Start time in seconds. Defaults to None.
+      end (int, optional): End time in seconds. Defaults to None.
+  """
+  try:
+    video = ffmpeg.input(video_path)
+    audio = ffmpeg.input(audio_path)
+
+    (
+      ffmpeg
+      .output(
+        video,
+        audio,
+        output_path,
+        vcodec='copy',
+        acodec='aac',
+        strict='experimental',
+
+        # ss=str(start) if start else '0',
+        # to=str(end) if end else None,
+        )
+        .overwrite_output()
+      .run(quiet=True)
+    )
+
+  except ffmpeg.Error as e:
+    raise Exception(f"ffmpeg error: {str(e)}")
